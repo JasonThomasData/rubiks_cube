9b0502a26ba33b86b52a6a3404008eecb2dc27c6
Jason Thomas <jason.thomas@myob.com>
2018-10-31 21:12:58 +1100

    Added some new funtionality to make the program more interesting

--- maker/main.js
+++ maker/main.js
@@ -1,19 +1,72 @@
 var screenHeight = window.screen.availHeight * 0.66;
+var cube = TTk.AlgorithmPuzzle(3);
+var cumulativeSeedAlgorithm = "";
+var sides = ["F", "B", "U", "D", "R", "L"]
 
-function drawCube() {
-    var algorithm = document.getElementById("algorithm").value;
+function getRandomSide() {
+    return sides[Math.floor(Math.random() * sides.length)];
+}
+
+function getInverse() {
+    var inverseChance = Math.round(Math.random());
+    if (inverseChance == 1) {
+        return "'";
+    }
+    return ""
+}
+
+function getRandomMove() {
+    var randomSide = getRandomSide();
+    var inverse = getInverse();
+    return randomSide + inverse;
+}
+
+function getNewRandomAlgorithm(numberOfTurns) {
+    var algorithm = " ";
+    for(var i = 0; i < numberOfTurns; i++) {
+        var move = getRandomMove();
+        algorithm = algorithm + move + " ";
+    }
+    return algorithm;
+}
+
+function makeNewAlgorithm(algorithm) {
+    return cumulativeSeedAlgorithm + algorithm;
+}
+
+function randomiseCube(numberOfTurns) {
+    var algorithm = getNewRandomAlgorithm(+numberOfTurns);
+    cumulativeSeedAlgorithm = makeNewAlgorithm(algorithm);
+}
+
+function drawCube(userAlgorithm) {
+    var algorithm = makeNewAlgorithm(userAlgorithm);
     var showAlgorithm = document.getElementById("show_algorithm").checked;
     document.getElementById("cube_viz").innerHTML = "";
-    var cube = TTk.AlgorithmPuzzle(3)
-        .showAlg(showAlgorithm)
+    cube.showAlg(showAlgorithm)
         .size({width:screenHeight, height:screenHeight})
         .alg(algorithm)
         ('#cube_viz');
-    cube.moveInterate()
+    cube.moveInteract()
         .mouse(false)
         .rotate(true);
 }
 
 document.getElementById("read").onclick = function() {
-    drawCube();
+    var userAlgorithm = document.getElementById("algorithm").value;
+    drawCube(userAlgorithm);
+}
+
+var randomiseButtons = document.getElementsByClassName("randomise");
+Array.from(randomiseButtons).forEach(function(button) {
+    button.onclick = function() {
+        var turns = button.value;
+        randomiseCube(turns);
+        drawCube("");
+    }
+})
+
+document.getElementById("reset").onclick = function() {
+    cumulativeSeedAlgorithm = ""
+    drawCube("");
 }
